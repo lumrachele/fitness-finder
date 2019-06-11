f35cc46b19093d51d937a7a432d7503daf3bf643
lumrachele <rlum894@gmail.com>
2019-06-11 13:43:00 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users
-      # resources :events
-      # resources :favorites
+      resources :users, only: [:index, :show, :update]
+      resources :events, only: [:index, :create, :show, :update, :destroy]
+      resources :favorites, only: [:create, :show, :destroy]
     end
   end
 
